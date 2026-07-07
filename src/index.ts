bd8adc5e89205ac2305c0f2099f080b8a2e93887
cascateer <cascateer@gmail.com>
2026-07-07 11:27:17 +0200

    export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { defaults } from "./defaults";
 export { File } from "./file";
 export { createFileTable, createTable } from "./table";
-export { FileTableRecord } from "./types";
+export { FileTableRecord, type TableAction } from "./types";
