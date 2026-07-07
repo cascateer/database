279ea09303b48f315867b926b0a7efcd61807c44
cascateer <cascateer@gmail.com>
2026-07-07 07:33:25 +0200

    minor

--- src/observables/reduceActions.ts
+++ src/observables/reduceActions.ts
@@ -3,7 +3,6 @@ import { flatMap } from "@cascateer/lib/observable";
 import { LazyPromise } from "@cascateer/lib/promise";
 import { Function1, last, noop, tap } from "lodash";
 import { mergeAll, OperatorFunction, scan, startWith } from "rxjs";
-import { v4 } from "uuid";
 import { TableAction, TableActionCreator } from "../types";
 
 export const reduceActions =
@@ -21,33 +20,44 @@ export const reduceActions =
     source.pipe(
       startWith(seed),
       scan(
-        (result, actions, actionsIndex) =>
-          result.then(({ records, previousActionId }) =>
+        (result, actions) =>
+          result.then(({ records, previousActionId, isInitialized }) =>
             actions.start(records).then(({ actions, callback }) => {
               const transformedRecords = tap(
                 transform(records, ...actions),
                 callback ?? noop,
               );
 
+              const lastActionId = last(actions)?.id ?? previousActionId;
+
+              if (!isInitialized && 0 in actions) {
+                if (actions[0].previousId != null) {
+                  throw new Error();
+                }
+
+                return {
+                  records: transformedRecords,
+                  actions: [
+                    {
+                      id: actions[0].id,
+                      type: "insert",
+                      payload: {
+                        records: transformedRecords,
+                      },
+                    },
+                  ],
+                  previousActionId: lastActionId,
+                  isInitialized: true,
+                };
+              }
+
               return {
                 records: transformedRecords,
-                actions:
-                  actionsIndex > 0
-                    ? actions.map((action, actionIndex, actions) => ({
-                        ...action,
-                        previousId:
-                          actions[actionIndex - 1]?.id ?? previousActionId,
-                      }))
-                    : [
-                        {
-                          id: v4(),
-                          type: "insert",
-                          payload: {
-                            records: transformedRecords,
-                          },
-                        },
-                      ],
-                previousActionId: last(actions)?.id ?? previousActionId,
+                actions: actions.map((action, actionIndex, actions) => ({
+                  ...action,
+                  previousId: actions[actionIndex - 1]?.id ?? previousActionId,
+                })),
+                previousActionId: lastActionId,
               };
             }),
           ),
@@ -55,6 +65,7 @@ export const reduceActions =
           records: Array<R>;
           actions: Array<TableAction<R, K>>;
           previousActionId?: string;
+          isInitialized?: boolean;
         }>({
           records: [],
           actions: [],
